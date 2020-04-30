be85d30bf4eac530dfa3983090ab88c0af616638
easyadin <tobiayokunnu@gmail.com>
2020-04-30 15:31:02 +0400

    menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { OnInit, ViewChild } from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
 import {ChangeDetectorRef, OnDestroy} from '@angular/core';
-
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,10 @@ import {ChangeDetectorRef, OnDestroy} from '@angular/core';
 })
 export class AppComponent {
   title = 'myPOD-Dashboard';
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
-
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
@@ -22,9 +22,17 @@ export class AppComponent {
     this.mobileQuery.addListener(this._mobileQueryListener);
    }
 
-
    ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
+  closeSidenav(){
+    if(this.mobileQuery.matches){
+      this.sidenav.close();
+    }
+  }
+
+  openSidenav(){
+    this.sidenav.open();
+  }
 }
